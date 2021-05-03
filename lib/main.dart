862b8602cb7005c93e98df7a1a22b7e223078d29
me-aditya <adityarathi1212@gmail.com>
2021-05-03 15:51:50 +0530

    Starter file with UI initialized

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+      
+    );
+  }
+}
